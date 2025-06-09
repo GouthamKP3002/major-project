2923870b212664881c713c2771238ab73f6cfc2d
Goutham K P <kpgoutham16@gmail.com>
2025-06-09 19:22:51 +0530

    app initiate

---
+++ Virtual P.O.V/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className='text-center text-2xl text-amber-950 bg-black'>fuck you</h1>
+    </div>
+  )
+}
+
+export default App
